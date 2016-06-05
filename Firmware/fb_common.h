2cad73e3dba066e4a7238eef4a9e08e448395d6c
marcfeifel <marc@feifel.com>
2016-06-05 13:17:28 -0700

    Establish new baseline.
    Works with multiple projects set for multiple programmers (one for
    master; one for remote).

--- Firmware/fb_common.h
+++ Firmware/fb_common.h
@@ -9,18 +9,8 @@
 //#define NODEID_REMOTE2 5
 //#define NODEID_REMOTE3 6
 
-typedef unsigned char  uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned long  uint32_t;
-typedef char  int8_t;
-typedef short int16_t;
-typedef long  int32_t;
-typedef bit bool;
-
 #define HIGH 1
 #define LOW 0
-#define false 0
-#define true (!false)
 
 typedef uint16_t CRC16_t;
 
