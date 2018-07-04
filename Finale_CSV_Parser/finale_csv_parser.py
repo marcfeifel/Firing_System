c64acaeb2a8ff4c38cbff0bb542cc174b0452a0f
marcfeifel <marc@feifel.com>
2018-07-04 08:51:09 -0700

    Better printouts
    And a preliminary version of the program.

--- Finale_CSV_Parser/finale_csv_parser.py
+++ Finale_CSV_Parser/finale_csv_parser.py
@@ -2,51 +2,56 @@ import csv
 
 program = [-1] * 128 * 8
 
-with open('sample.csv', newline='') as csvfile:
+with open('program_2017_prelim.csv', newline='') as csvfile:
     spamreader = csv.DictReader(csvfile, dialect='excel')
     count = 1
+    duplicates = 0;
+    missing_data = 0;
     for row in spamreader:
         count = count + 1
-
-        if row['Effect Name'] != '':
-            name = row['Effect Name']
-        else:
-            print(count, " - Missing effect name")
-            continue
 
         if row['Ignition Event Time'] != '':
             ignition_time = int(float(row['Ignition Event Time']) * 1000)
         else:
             print(count, " - Missing ignition time")
+            missing_data = missing_data + 1
             continue
 
         if row['Module Address'] != '':
             addr_module = int(row['Module Address'])
         else:
             print(count, " - Missing module address")
+            missing_data = missing_data + 1
             continue
 
         if row['Slat Address'] != '':
             addr_slat = int(row['Slat Address'])
         else:
             print(count, " - Missing slat address")
+            missing_data = missing_data + 1
             continue
 
         if row['Pin Address'] != '':
             addr_pin = int(row['Pin Address'])
         else:
             print(count, " - Missing pin address")
+            missing_data = missing_data + 1
             continue
+
+        #print(count, " - added %s at %u, %u, %u" % (row['Effect Name'], addr_module, addr_slat, addr_pin))
 
         index = addr_module*128 + addr_slat*16 + addr_pin
 
         if program[index] == -1:
             program[index] = ignition_time
         elif program[index] == ignition_time:
-            print()
+            print(end='')
         else:
             print(count, " - Duplicate with differing firing time: ", addr_module, ',', addr_slat, ',', addr_pin)
+            duplicates = duplicates + 1
 
+
+    added = 0
     print("")
     print("")
     print("/*  Module 0  ,   Module 1  ,   Module 2  ,   Module 3  ,   Module 4  ,   Module 5   */")
@@ -58,6 +63,7 @@ with open('sample.csv', newline='') as csvfile:
                 val = program[index]
                 if val != -1:
                     print("%12u" % (program[index]), end='')
+                    added = added + 1
                 else:
                     print("(uint32_t)-1", end='')
 
@@ -69,4 +75,8 @@ with open('sample.csv', newline='') as csvfile:
             else:
                 print(",", end='')
             print(" /* Socket %u Cue %u */" % (socket, cue))
+    print("")    
     print("")
+    print(duplicates, "duplicate cues with different firing times.")
+    print(missing_data, "cues found with missing firing data.")
+    print(added, "cues added to program.")
