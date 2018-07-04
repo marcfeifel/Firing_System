e993af971d6cbc3148c6b71c14341c3e78ef4f7d
marcfeifel <marc@feifel.com>
2018-07-04 09:07:41 -0700

    More helpful diagnostics.

--- Finale_CSV_Parser/finale_csv_parser.py
+++ Finale_CSV_Parser/finale_csv_parser.py
@@ -1,6 +1,7 @@
 import csv
 
 program = [-1] * 128 * 8
+entries = [0] * 128 * 8
 
 with open('program_2017_prelim.csv', newline='') as csvfile:
     spamreader = csv.DictReader(csvfile, dialect='excel')
@@ -13,41 +14,42 @@ with open('program_2017_prelim.csv', newline='') as csvfile:
         if row['Ignition Event Time'] != '':
             ignition_time = int(float(row['Ignition Event Time']) * 1000)
         else:
-            print(count, " - Missing ignition time")
+            print("Row", count, " - Missing ignition time")
             missing_data = missing_data + 1
             continue
 
         if row['Module Address'] != '':
             addr_module = int(row['Module Address'])
         else:
-            print(count, " - Missing module address")
+            print("Row", count, " - Missing module address")
             missing_data = missing_data + 1
             continue
 
         if row['Slat Address'] != '':
             addr_slat = int(row['Slat Address'])
         else:
-            print(count, " - Missing slat address")
+            print("Row", count, " - Missing slat address")
             missing_data = missing_data + 1
             continue
 
         if row['Pin Address'] != '':
             addr_pin = int(row['Pin Address'])
         else:
-            print(count, " - Missing pin address")
+            print("Row", count, " - Missing pin address")
             missing_data = missing_data + 1
             continue
 
-        #print(count, " - added %s at %u, %u, %u" % (row['Effect Name'], addr_module, addr_slat, addr_pin))
+        #print("Row", count, "added %s at %u, %u, %u" % (row['Effect Name'], addr_module, addr_slat, addr_pin))
 
         index = addr_module*128 + addr_slat*16 + addr_pin
 
         if program[index] == -1:
             program[index] = ignition_time
+            entries[index] = count
         elif program[index] == ignition_time:
             print(end='')
         else:
-            print(count, " - Duplicate with differing firing time: ", addr_module, ',', addr_slat, ',', addr_pin)
+            print("Row", count, " - Duplicate with row", entries[index], "(differerent firing times", program[index]/1000, "vs", ignition_time/1000, "): ", addr_module, ',', addr_slat, ',', addr_pin)
             duplicates = duplicates + 1
 
 
