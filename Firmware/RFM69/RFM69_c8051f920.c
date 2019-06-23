b0d015238e7396286465dfccdd50d286f2656644
marcfeifel <marc@feifel.com>
2019-06-23 14:22:33 -0700

    Select default program; formatting.

--- Firmware/RFM69/RFM69_c8051f920.c
+++ Firmware/RFM69/RFM69_c8051f920.c
@@ -3,36 +3,36 @@
 
 uint8_t RFM69_SPI_transfer(uint8_t out)
 {
-  // clear the interrupt flag
-  SPIF0 = 0;
+    // clear the interrupt flag
+    SPIF0 = 0;
+    
+    // write the address
+    SPI0DAT = out;
 
-  // write the address
-  SPI0DAT = out;
+    while (!SPIF0)
+    {
+        // wait for the xfer to complete
+    }
 
-  while (!SPIF0)
-  {
-    // wait for the xfer to complete
-  }
-
-  return SPI0DAT;
+    return SPI0DAT;
 }
 
 
 // select the RFM69 transceiver (save SPI settings, set CS low)
 void RFM69_SPI_select() {
-  NSS0MD0 = LOW;
+    NSS0MD0 = LOW;
 }
 
 
 // unselect the RFM69 transceiver (set CS high, restore SPI settings)
 void RFM69_SPI_unselect() {
-  NSS0MD0 = HIGH;
+    NSS0MD0 = HIGH;
 }
 
 void RFM69_assert_reset() {
-  PIN_RFM69HW_RST_O = 1;
+    PIN_RFM69HW_RST_O = 1;
 }
 
 void RFM69_deassert_reset() {
-  PIN_RFM69HW_RST_O = 0;
+    PIN_RFM69HW_RST_O = 0;
 }
